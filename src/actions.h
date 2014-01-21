87d490efc3a72d752db36222c13e8dd619eeccfd
Igor Chaika <kp580bm1@gmail.com>
2014-01-21 20:43:31 +0200

    Docs for AI and actions.

--- src/actions.h
+++ src/actions.h
@@ -3,131 +3,215 @@
 #include "info.h"
 #include "point.h"
 
-namespace Chthon {
+namespace Chthon { /// @defgroup Actions Actions
+/// @{
 
 class Game;
 class Monster;
 
+/** Basic action class. Represents game action.
+ * New action is created by subclassing Action and overriding its pure virtual function commit().
+ *
+ * Action should be constructed in order to commit:
+ * @code{.cpp}
+ * Grab action;
+ * action.commit(player, game);
+ * @endcode
+ *
+ * GameEvent class serves as the Exception (used by assert()).
+ * @see GameEvent
+ */
 class Action {
 public:
 	typedef GameEvent Exception;
 
 	virtual ~Action() {}
+	/** Commit action.
+	 * @param someone monster, which is commiting action.
+	 * @param game reference to the game.
+	 */
 	virtual void commit(Monster & someone, Game & game) = 0;
+	/** Evaluates and asserts game related expressions.
+	 * If expression evaluates to false, throws corresponding Exception of exception_type and subject and object parameters.
+	 * @see GameEvent
+	 * @see GameEvent::EventType
+	 */
 	void assert(bool expression, GameEvent::EventType exception_type, const Info & subject, const Info & object = Info());
 };
 
+/** Directed action - subclass of basic Action.
+ * It is supplied with direction (or shift) in form of Point.
+ */
 class DirectedAction : public Action {
 public:
 	const Point shift;
 
+	/// Constructs directed action using given direction.
 	DirectedAction(const Point & action_direction) : shift(action_direction) {}
 	virtual ~DirectedAction() {}
 };
 
+/** Slot action - subclass of basic actions.
+ * It represents all actions with monster's inventory.
+ * It is supplied with inventory slot.
+ */
 class SlotAction : public Action {
 public:
 	unsigned slot;
 
+	/// Constructs action using given inventory slot.
 	SlotAction(unsigned action_slot) : slot(action_slot) {}
 	virtual ~SlotAction() {}
 };
 
 
+/** Do nothing.
+ */
 class Wait : public Action {
 public:
 	virtual void commit(Monster &, Game &) {}
 };
 
+/** Bump into monsters, impassable objects or cells.
+ * Otherwise move freely.
+ */
 class Move : public DirectedAction {
 public:
 	Move(const Point & action_shift) : DirectedAction(action_shift) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Open closed doors and take items from container.
+ * Unlocks locked doors if corresponding key is present.
+ */
 class Open : public DirectedAction {
 public:
 	Open(const Point & direction) : DirectedAction(direction) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Close opened doors.
+ */
 class Close : public DirectedAction {
 public:
 	Close(const Point & direction) : DirectedAction(direction) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Swing in specified direction.
+ * Hit monster, object or cell, if present.
+ * Also swinging opens closed (but not locked) doors.
+ */
 class Swing : public DirectedAction {
 public:
 	Swing(const Point & direction) : DirectedAction(direction) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Throw wielded object in specified direction.
+ * Hit monsters, object and opaque cells.
+ * If object is container or fountain, item falls into it.
+ * For fountains this means that item is forever lost.
+ */
 class Fire : public DirectedAction {
 public:
 	Fire(const Point & direction) : DirectedAction(direction) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Drink (and heal, if needed) from drinkable object.
+ */
 class Drink : public DirectedAction {
 public:
 	Drink(const Point & direction) : DirectedAction(direction) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Grab item from the floor.
+ */
 class Grab : public Action {
 public:
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Drop item on the floor.
+ * If it is wielded or worn, it will be taken off or unwielded first.
+ */
 class Drop : public SlotAction {
 public:
 	Drop(unsigned action_slot) : SlotAction(action_slot) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Wield item. If it was worn, it will be taken off first.
+ */
 class Wield : public SlotAction {
 public:
 	Wield(unsigned action_slot) : SlotAction(action_slot) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Unwield wielded item.
+ */
 class Unwield : public Action {
 public:
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Wear wearable item. If it was wielded, it will be unwielded first.
+ */
 class Wear : public SlotAction {
 public:
 	Wear(unsigned action_slot) : SlotAction(action_slot) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Take off worn item.
+ */
 class TakeOff : public Action {
 public:
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Eat edible item.
+ * If its worn, it will be taken off first.
+ * If its wielded, it will be unwielded first (except for refillable items).
+ * Item may heal monster or cure poison.
+ * If item can be refilled, it is turns empty, otherwise it is lost.
+ */
 class Eat : public SlotAction {
 public:
 	Eat(unsigned action_slot) : SlotAction(action_slot) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Go up using stairs.
+ * If it is exit from the dungeon, one must carry a quest item in order to exit.
+ * Otherwise he will be reminded of that.
+ */
 class GoUp : public Action {
 public:
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Go down using stairs.
+ * If it is exit from the dungeon, one must carry a quest item in order to exit.
+ * Otherwise he will be reminded of that.
+ */
 class GoDown : public Action {
 public:
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/** Put item into object or on the floor.
+ * If object is drinkable and item can be refilled, it is refilled.
+ * Otherwise it is dropped on the floor.
+ */
 class Put : public DirectedAction {
 public:
 	Put(const Point & direction) : DirectedAction(direction) {}
 	virtual void commit(Monster & someone, Game & game);
 };
 
+/// @}
 }
--- src/ai.h
+++ src/ai.h
@@ -2,37 +2,62 @@
 #include <map>
 #include <vector>
 
-namespace Chthon {
+namespace Chthon { /// @defgroup AI AI
+/// @{
 
 class Game;
 class Monster;
 class Action;
 
+/** Basic controller class. Represents monster behaviour.
+ * Subclass it and override pure virtual act() function in order to define new behaviour.
+ */
 class Controller {
 public:
 	virtual ~Controller() {}
+	/** Produces some Action basing on its state and game environment.
+	 * Note that this function should just construct Action, which then must be commited.
+	 */
 	virtual Action * act(Monster &, Game &) = 0;
 };
 
+/** Simple AI with some basic, not very wit monster needs - walk, attack on sight etc.
+ *
+ * AI is constructed using chain of commands. If first command wasn't succeed, go to the next one, and so one.
+ * Last command is always WAIT.
+ * @code{.cpp}
+ * BasicAI * ai = new BasicAI();
+ * ai->add(MOVE_TO_HIT_PLAYER_IF_SEES)->add(MOVE_RANDOM)->add(WAIT);
+ * @endcode
+ */
 class BasicAI : public Controller {
 public:
 	enum {
-		WAIT, MOVE_TO_HIT_PLAYER_IF_SEES, HIT_PLAYER_IF_NEAR, MOVE_RANDOM,
+		WAIT, ///< Do nothing. Always succeeds.
+		MOVE_TO_HIT_PLAYER_IF_SEES, ///< If player monster is in sight range, move to it, otherwise fail.
+		HIT_PLAYER_IF_NEAR, ///< If player is in the reach, hit it. Otherwise fail.
+		MOVE_RANDOM, ///< Move in random direction. Always succeeds.
 		COUNT
 	};
 	virtual ~BasicAI() {}
+	/// Adds next command to the chain.
 	BasicAI * add(unsigned action);
+	/// Produces action using set chain of commands.
 	virtual Action * act(Monster & monster, Game & game);
 private:
 	std::vector<unsigned> actions;
 };
 
+/// Stores controllers by its id.
 struct ControllerFactory {
 	~ControllerFactory();
+	/// Add new controller under given id.
 	void add_controller(int ai, Controller * controller);
+	/// Get controller by its id.
 	Controller * get_controller(int ai) const;
 private:
 	std::map<int, Controller*> controllers;
 };
 
+/// @}
 }
