001c2d3c696bb0c993e29521c5dfbd76d490cab7
Igor Chaika <kp580bm1@gmail.com>
2014-02-10 12:12:02 +0200

    Map: default constructor and resize method.

--- src/map.h
+++ src/map.h
@@ -15,7 +15,7 @@ public:
 
 	/// Constructs Map with specified size.
 	/// All values is constructed using default constructor.
-	Map(unsigned map_width, unsigned map_height)
+	Map(unsigned map_width = 0, unsigned map_height = 0)
 		: w(map_width), h(map_height), cells(w * h, T()) {}
 	~Map() {}
 
@@ -35,7 +35,7 @@ public:
 	/// Checks if point (x, y) is valid, i.e. inside the map bounds.
 	bool valid(int x, int y) const
 	{
-		return (x >= 0 && x < int(w) && y >= 0 && y < int(h));
+		return (0 <= x && x < int(w) && 0 <= y && y < int(h));
 	}
 	/// Checks if point (x, y) is valid, i.e. inside the map bounds.
 	bool valid(const Point & pos) const { return valid(pos.x, pos.y); }
@@ -58,6 +58,34 @@ public:
 	/// Returns cell at the specified position.
 	/// If position is invalid, behaviour is undefined;
 	T & cell(const Point & pos) { return cell(pos.x, pos.y); }
+
+	void resize(unsigned new_width, unsigned new_height)
+	{
+		typename std::vector<T> new_pixels(new_width * new_height, 0);
+		typename std::vector<T>::const_iterator src = cells.begin();
+		typename std::vector<T>::iterator dest = new_pixels.begin();
+		unsigned min_common_width = std::min(w, new_width);
+		unsigned current_col = 0;
+		unsigned rows_left_to_copy = std::min(h, new_height);
+		int src_row_inc = w > new_width ? int(w) - int(new_width) : 0;
+		int dest_row_inc = w < new_width ? int(new_width) - int(w) : 0;
+		while(rows_left_to_copy) {
+			*dest++ = *src++;
+			++current_col;
+			if(current_col == min_common_width) {
+				--rows_left_to_copy;
+				if(!rows_left_to_copy) {
+					break;
+				}
+				src += src_row_inc;
+				dest += dest_row_inc;
+				current_col = 0;
+			}
+		}
+		cells.swap(new_pixels);
+		w = new_width;
+		h = new_height;
+	}
 private:
 	unsigned w, h;
 	std::vector<T> cells;
--- test/map_test.cpp
+++ test/map_test.cpp
@@ -40,4 +40,33 @@ TEST(should_fill_map_with_multi_values)
 	EQUAL(map.cell(1, 1), 4);
 }
 
+TEST(should_resize_map)
+{
+	Map<int> map(2, 2);
+	map.resize(4, 4);
+	EQUAL(map.width(), 4u);
+	EQUAL(map.height(), 4u);
+}
+
+TEST(should_keep_existing_values_when_resizing_map)
+{
+	Map<int> map(2, 2);
+	int a[] = {0, 1, 2, 3};
+	std::copy(a, a + 4, map.begin());
+	map.resize(4, 4);
+	EQUAL(map.cell(0, 0), 0u);
+	EQUAL(map.cell(1, 0), 1u);
+	EQUAL(map.cell(0, 1), 2u);
+	EQUAL(map.cell(1, 1), 3u);
+}
+
+TEST(should_fill_new_cells_with_default_values_when_resizing_map)
+{
+	Map<int> map(2, 2);
+	std::fill(map.begin(), map.end(), 1);
+	map.resize(2, 3);
+	EQUAL(map.cell(0, 2), 0u);
+	EQUAL(map.cell(1, 2), 0u);
+}
+
 }
